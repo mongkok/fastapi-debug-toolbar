7a65831069c0b19132236cf23e51ec443b4e82aa
mongkok <domake.io@gmail.com>
2021-07-24 02:13:59 +0700

    Removed setup.cfg

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -4,6 +4,6 @@ set -e
 set -x
 
 mypy debug_toolbar tests --install-type --non-interactive
-flake8 debug_toolbar tests
+flake8 debug_toolbar tests --max-line-length 88
 black debug_toolbar tests --check
 isort debug_toolbar tests --check-only
