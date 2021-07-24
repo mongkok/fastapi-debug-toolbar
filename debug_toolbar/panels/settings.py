9eb21c9f3b1b46a80ef3914989e0ab982474b4a8
mongkok <domake.io@gmail.com>
2021-07-25 05:29:14 +0700

    Moved down project settings

--- debug_toolbar/panels/settings.py
+++ debug_toolbar/panels/settings.py
@@ -1,22 +1,6 @@
-import typing as t
-
-from fastapi import Request, Response
-
 from debug_toolbar.panels import Panel
 
 
 class SettingsPanel(Panel):
     title = "Settings"
     template = "panels/settings.html"
-
-    async def generate_stats(
-        self,
-        request: Request,
-        response: Response,
-    ) -> t.Optional[t.Dict[str, t.Any]]:
-        return {
-            "app_settings": {
-                **self.toolbar.settings.SETTINGS,
-                "Debug Toolbar": self.toolbar.settings,
-            },
-        }
