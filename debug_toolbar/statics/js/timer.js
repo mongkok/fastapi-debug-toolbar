049aff0f9934cd2b8ba2309ea0c96da2d3587696
mongkok <5514990+mongkok@users.noreply.github.com>
2022-12-10 20:00:28 +0700

    Added JS code formatter

--- debug_toolbar/statics/js/timer.js
+++ debug_toolbar/statics/js/timer.js
@@ -1,74 +1,73 @@
 import { $$ } from "./utils.js";
 
 function insertBrowserTiming() {
-    const timing = performance.timing,
-        timingOffset = timing.navigationStart,
-        totalTime = timing.loadEventEnd - timingOffset;
+  const timing = performance.timing,
+    timingOffset = timing.navigationStart,
+    totalTime = timing.loadEventEnd - timingOffset;
 
-    function getLeft(stat) {
-        return ((timing[stat] - timingOffset) / totalTime) * 100.0;
+  function getLeft(stat) {
+    return ((timing[stat] - timingOffset) / totalTime) * 100.0;
+  }
+  function getCSSWidth(stat, endStat) {
+    let width = ((timing[endStat] - timing[stat]) / totalTime) * 100.0;
+    // Calculate relative percent
+    width = (100.0 * width) / (100.0 - getLeft(stat));
+    return width < 1 ? "2px" : `${width || 0}%`;
+  }
+  function addRow(tbody, stat, endStat) {
+    const row = document.createElement("tr");
+    if (endStat) {
+      // Render a start through end bar
+      row.innerHTML = `
+        <td>${stat.replace("Start", "")}</td>
+        <td>
+            <svg class="fastDebugLineChart"
+                xmlns="http://www.w3.org/2000/svg"
+                viewbox="0 0 100 5"
+                preserveAspectRatio="none">
+                <rect y="0" height="5" fill="#ccc" />
+            </svg>
+        </td>
+        <td>
+            ${timing[stat] - timingOffset}
+            (${timing[endStat] - timing[stat]})
+        </td>`;
+      row.querySelector("rect").setAttribute("width", getCSSWidth(stat, endStat));
+    } else {
+      // Render a point in time
+      row.innerHTML = `
+        <td>${stat}</td>
+        <td>
+            <svg class="fastDebugLineChart"
+                xmlns="http://www.w3.org/2000/svg"
+                viewbox="0 0 100 5"
+                preserveAspectRatio="none">
+                <rect y="0" height="5" fill="#ccc" />
+            </svg>
+        </td>
+        <td>${timing[stat] - timingOffset}</td>`;
+      row.querySelector("rect").setAttribute("width", 2);
     }
-    function getCSSWidth(stat, endStat) {
-        let width = ((timing[endStat] - timing[stat]) / totalTime) * 100.0;
-        // Calculate relative percent
-        width = (100.0 * width) / (100.0 - getLeft(stat));
-        return width < 1 ? "2px" : `${width||0}%`;
-    }
-    function addRow(tbody, stat, endStat) {
-        const row = document.createElement("tr");
-        if (endStat) {
-            // Render a start through end bar
-            row.innerHTML = `
-            <td>${stat.replace("Start", "")}</td>
-            <td>
-                <svg class="fastDebugLineChart"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewbox="0 0 100 5"
-                    preserveAspectRatio="none">
-                    <rect y="0" height="5" fill="#ccc" />
-                </svg>
-            </td>
-            <td>
-                ${timing[stat] - timingOffset}
-                (${timing[endStat] - timing[stat]})
-            </td>`;
-            row.querySelector("rect")
-                .setAttribute("width", getCSSWidth(stat, endStat));
-        } else {
-            // Render a point in time
-            row.innerHTML = `
-            <td>${stat}</td>
-            <td>
-                <svg class="fastDebugLineChart"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewbox="0 0 100 5"
-                    preserveAspectRatio="none">
-                    <rect y="0" height="5" fill="#ccc" />
-                </svg>
-            </td>
-            <td>${timing[stat] - timingOffset}</td>`;
-            row.querySelector("rect").setAttribute("width", 2);
-        }
-        row.querySelector("rect").setAttribute("x", getLeft(stat));
-        tbody.appendChild(row);
-    }
+    row.querySelector("rect").setAttribute("x", getLeft(stat));
+    tbody.appendChild(row);
+  }
 
-    const browserTiming = document.getElementById("fastDebugBrowserTiming");
-    // Determine if the browser timing section has already been rendered.
-    if (browserTiming.classList.contains("fastdt-hidden")) {
-        const tbody = document.getElementById("fastDebugBrowserTimingTableBody");
-        // This is a reasonably complete and ordered set of
-        // timing periods (2 params) and events (1 param)
-        addRow(tbody, "domainLookupStart", "domainLookupEnd");
-        addRow(tbody, "connectStart", "connectEnd");
-        addRow(tbody, "requestStart", "responseEnd"); // There is no requestEnd
-        addRow(tbody, "responseStart", "responseEnd");
-        addRow(tbody, "domLoading", "domComplete"); // Spans the events below
-        addRow(tbody, "domInteractive");
-        addRow(tbody, "domContentLoadedEventStart", "domContentLoadedEventEnd");
-        addRow(tbody, "loadEventStart", "loadEventEnd");
-        browserTiming.classList.remove("fastdt-hidden");
-    }
+  const browserTiming = document.getElementById("fastDebugBrowserTiming");
+  // Determine if the browser timing section has already been rendered.
+  if (browserTiming.classList.contains("fastdt-hidden")) {
+    const tbody = document.getElementById("fastDebugBrowserTimingTableBody");
+    // This is a reasonably complete and ordered set of
+    // timing periods (2 params) and events (1 param)
+    addRow(tbody, "domainLookupStart", "domainLookupEnd");
+    addRow(tbody, "connectStart", "connectEnd");
+    addRow(tbody, "requestStart", "responseEnd"); // There is no requestEnd
+    addRow(tbody, "responseStart", "responseEnd");
+    addRow(tbody, "domLoading", "domComplete"); // Spans the events below
+    addRow(tbody, "domInteractive");
+    addRow(tbody, "domContentLoadedEventStart", "domContentLoadedEventEnd");
+    addRow(tbody, "loadEventStart", "loadEventEnd");
+    browserTiming.classList.remove("fastdt-hidden");
+  }
 }
 
 const fastDebug = document.getElementById("fastDebug");
--- debug_toolbar/statics/js/versions.js
+++ debug_toolbar/statics/js/versions.js
@@ -1,104 +1,95 @@
 import { $$ } from "./utils.js";
 
 function pypiIndex() {
-    const loader = document
-        .getElementById("VersionsPanel")
-        .querySelector(".fastdt-loader");
+  const loader = document.getElementById("VersionsPanel").querySelector(".fastdt-loader");
 
-    function versionInfo(releases, version) {
-        return `
-            <code>${version}</code>
-            <span class="fast-date">
-                ${new Date(releases[version]).toLocaleDateString()}
-            </span>`;
+  function versionInfo(releases, version) {
+    return `
+      <code>${version}</code>
+      <span class="fast-date">
+          ${new Date(releases[version]).toLocaleDateString()}
+      </span>`;
+  }
+  function link(url) {
+    return `
+      <a href="${url}" target="_blank">
+          ${$$.truncatechars(url || "", 40)}
+      </a>`;
+  }
+  function render(rowVersion, data) {
+    if (rowVersion.textContent !== data.version) {
+      rowVersion.parentNode.style.backgroundColor = loader.nextElementSibling.dataset.warningColor;
     }
-    function link(url) {
-        return `
-            <a href="${url}" target="_blank">
-                ${$$.truncatechars(url, 40)}
-            </a>`;
+    if (data.releases[rowVersion.textContent] !== null) {
+      rowVersion.innerHTML = versionInfo(data.releases, rowVersion.textContent);
     }
-    function render(rowVersion, data) {
-        if (rowVersion.textContent !== data.version) {
-            rowVersion.parentNode.style.backgroundColor =
-                loader.nextElementSibling.dataset.warningColor;
-        }
-        if (data.releases[rowVersion.textContent] !== null) {
-            rowVersion.innerHTML =
-                versionInfo(data.releases, rowVersion.textContent);
-        }
-        const lastVersion = rowVersion.nextElementSibling;
-        lastVersion.innerHTML = versionInfo(data.releases, data.version);
+    const lastVersion = rowVersion.nextElementSibling;
+    lastVersion.innerHTML = versionInfo(data.releases, data.version);
 
-        const python = lastVersion.nextElementSibling;
-        python.innerHTML = data.requires_python;
+    const python = lastVersion.nextElementSibling;
+    python.innerHTML = data.requires_python;
 
-        const status = python.nextElementSibling;
-        status.innerHTML = data.status ? data.status.slice(26) : "";
-        status.nextElementSibling.innerHTML = link(data.home_page);
-    }
-    function getData(pypi) {
-        return {
-            version: pypi.info.version,
-            requires_python: pypi.info.requires_python,
-            home_page: pypi.info.home_page,
-            releases: Object.fromEntries(
-                Object.entries(pypi.releases).map(
-                    function([k, v], i) {
-                        return [k, v.length ? v[0].upload_time : null];
-                    }
-                )
-            ),
-            status: pypi.info.classifiers.find(
-                function(classifier) {
-                    return classifier.startsWith("Development Status");
-                }
-            )
-        };
-    }
-    function updateRow(row) {
-        return new Promise((resolve) => {
-            const name = row.firstElementChild.textContent.trim();
-            const data = JSON.parse(localStorage.getItem(`pypi-${name}`));
-            const rowVersion = row.children.item(1);
+    const status = python.nextElementSibling;
+    status.innerHTML = data.status ? data.status.slice(26) : "";
+    status.nextElementSibling.innerHTML = link(data.home_page);
+  }
+  function getData(pypi) {
+    return {
+      version: pypi.info.version,
+      requires_python: pypi.info.requires_python,
+      home_page: pypi.info.home_page,
+      releases: Object.fromEntries(
+        Object.entries(pypi.releases).map(function ([k, v], i) {
+          return [k, v.length ? v[0].upload_time : null];
+        })
+      ),
+      status: pypi.info.classifiers.find(function (classifier) {
+        return classifier.startsWith("Development Status");
+      }),
+    };
+  }
+  function updateRow(row) {
+    return new Promise((resolve) => {
+      const name = row.firstElementChild.textContent.trim();
+      const data = JSON.parse(localStorage.getItem(`pypi-${name}`));
+      const rowVersion = row.children.item(1);
 
-            if (data === null || !(rowVersion.textContent in data.releases)) {
-                fetch(`https://pypi.org/pypi/${name}/json`)
-                    .then(function (response) {
-                        if (response.ok) {
-                            response.json().then(function (pypi) {
-                                const data = getData(pypi);
+      if (data === null || !(rowVersion.textContent in data.releases)) {
+        fetch(`https://pypi.org/pypi/${name}/json`).then(function (response) {
+          if (response.ok) {
+            response.json().then(function (pypi) {
+              const data = getData(pypi);
 
-                                if (!(rowVersion.textContent in data.releases)) {
-                                    data.releases[rowVersion.textContent] = null;
-                                }
-                                localStorage.setItem(`pypi-${name}`, JSON.stringify(data));
-                                render(rowVersion, data);
-                                resolve();
-                            });
-                        }
-                    });
-            } else {
-                render(rowVersion, data);
-                resolve();
-            }
+              if (!(rowVersion.textContent in data.releases)) {
+                data.releases[rowVersion.textContent] = null;
+              }
+              localStorage.setItem(`pypi-${name}`, JSON.stringify(data));
+              render(rowVersion, data);
+              resolve();
+            });
+          }
         });
-    }
-    if (loader && !loader.getAttribute("lock")) {
-        loader.setAttribute("lock", true);
+      } else {
+        render(rowVersion, data);
+        resolve();
+      }
+    });
+  }
+  if (loader && !loader.getAttribute("lock")) {
+    loader.setAttribute("lock", true);
 
-        const table = loader.nextElementSibling;
-        const queryResult = table.querySelectorAll("tbody > tr");
-        const promises = [];
+    const table = loader.nextElementSibling;
+    const queryResult = table.querySelectorAll("tbody > tr");
+    const promises = [];
 
-        for (let i = 0; i < queryResult.length; i++) {
-            promises.push(updateRow(queryResult[i]));
-        }
-        Promise.all(promises).then(() => {
-            loader.remove();
-            table.classList.remove("fastdt-hidden");
-        });
+    for (let i = 0; i < queryResult.length; i++) {
+      promises.push(updateRow(queryResult[i]));
     }
+    Promise.all(promises).then(() => {
+      loader.remove();
+      table.classList.remove("fastdt-hidden");
+    });
+  }
 }
 
 const fastDebug = document.getElementById("fastDebug");
