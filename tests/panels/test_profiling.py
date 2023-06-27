1583d6563d1aa556934c908dbdd5ab3ac3c5829c
mongkok <5514990+mongkok@users.noreply.github.com>
2023-06-27 17:57:35 +0700

    Fixed profiling test

--- tests/panels/test_profiling.py
+++ tests/panels/test_profiling.py
@@ -10,4 +10,4 @@ def test_profiling(client: TestClient, path: str) -> None:
     store_id = client.get_store_id(f"/{path}")
     stats = client.get_stats(store_id, "ProfilingPanel")
 
-    assert "profileSession" in stats["content"]
+    assert "pyinstrumentHTMLRenderer" in stats["content"]
