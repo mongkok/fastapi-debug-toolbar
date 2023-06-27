c6c842fa9ebb44fb5523d06afeaaabb6ae7f10ff
mongkok <5514990+mongkok@users.noreply.github.com>
2023-06-27 17:40:25 +0700

    Improved imports

--- tests/panels/sqlalchemy/database.py
+++ tests/panels/sqlalchemy/database.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 from sqlalchemy.pool import StaticPool
 
 engine = create_engine(
