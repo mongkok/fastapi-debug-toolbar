5abaefdd178ca7717dfd45cdf7f6aafc4de33cb9
mongkok <5514990+mongkok@users.noreply.github.com>
2024-02-13 12:04:40 +0700

    Add middleware tests

--- tests/test_middleware.py
+++ tests/test_middleware.py
@@ -1,22 +1,34 @@
-from fastapi import status
-from fastapi.testclient import TestClient
+from fastapi import FastAPI, Request, status
+
+from debug_toolbar.middleware import show_toolbar
+from debug_toolbar.settings import DebugToolbarSettings
+
+from .testclient import TestClient
 
 
 def test_sync(client: TestClient) -> None:
-    response = client.get("/sync")
-    assert response.status_code == status.HTTP_200_OK
+    assert client.get_store_id("/sync")
 
 
 def test_async(client: TestClient) -> None:
-    response = client.get("/async")
-    assert response.status_code == status.HTTP_200_OK
+    assert client.get_store_id("/async")
 
 
 def test_json(client: TestClient) -> None:
-    response = client.get("/openapi.json")
-    assert response.status_code == status.HTTP_200_OK
+    assert client.get_store_id("/openapi.json")
 
 
 def test_404(client: TestClient) -> None:
     response = client.get("/404")
     assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+def test_show_toolbar_not_allowed(app: FastAPI) -> None:
+    scope = {
+        "app": app,
+        "type": "http",
+        "client": ("invalid", 80),
+    }
+    request = Request(scope=scope)
+    settings = DebugToolbarSettings(ALLOWED_HOSTS=["test"])
+    assert not show_toolbar(request, settings)
